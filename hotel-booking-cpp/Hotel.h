be5645d7ff0e5787a3e9628218ba0d68db803214
Daniel <xd3nux@protonmail.com>
2023-12-08 15:32:35 +0100

    Added variables and methods for Garage and Hotel classes.

--- hotel-booking-cpp/Hotel.h
+++ hotel-booking-cpp/Hotel.h
@@ -1,3 +1,15 @@
-#pragma once
-class Hotel {};
+#ifndef HOTEL_H
+#define HOTEL_H
 
+class Hotel {
+private:
+	int roomNr;
+	int floor;
+public:
+	virtual int getFloor() = 0;
+	virtual void setFloor(int floor) = 0;
+	virtual int getRoom() = 0;
+	virtual void setRoom(int roomNr) = 0;
+}; 
+
+#endif
