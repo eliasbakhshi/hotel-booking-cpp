032226039fb53bb89f169165be606e89829a9558
Daniel <xd3nux@protonmail.com>
2023-12-29 16:14:22 +0100

    Created a functionality for differing between normal and VIP guests so that a personal 4-digits code is generated in case the user is asking for a VIP package.

--- hotel-booking-cpp/Spa.cpp
+++ hotel-booking-cpp/Spa.cpp
@@ -1,6 +1,33 @@
-#ifndef SPA_H
-#define SPA_H
+#include "Spa.h"
 
+Spa::Spa(bool isVip) {
+	this->isVip = isVip;
+}
 
+Spa::Spa() {
+	this->isVip = false;
+}
 
-#endif // !#define SPA.H
+Spa::~Spa() {
+}
+
+int Spa::generateCustomEntryCode() {
+	int entryCode;
+	if (this->isVip == true) {
+		srand(time(0));
+		entryCode = rand() % 10000;
+	}
+	else {
+		cout << "User is not VIP!";
+		entryCode = 0;
+	}
+	return entryCode;
+}
+
+void Spa::setIsVip(bool isVip) {
+	this->isVip = isVip;
+}
+
+bool Spa::getIsVip() {
+	return this->isVip;
+}
