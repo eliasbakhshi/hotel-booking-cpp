9475365a18d4e486dabed096415834a3566de9af
Daniel <xd3nux@protonmail.com>
2023-12-29 19:55:52 +0100

    Added options and functionalities that seperates a normal room from a VIP room, including minibar for normal room and the ability to choose room size for VIP guests and how many meals a day they want.

--- hotel-booking-cpp/NormalRoom.cpp
+++ hotel-booking-cpp/NormalRoom.cpp
@@ -4,3 +4,20 @@ NormalRoom::NormalRoom(int roomNum, bool isAvailable)
 	:Room(roomNum, isAvailable)
 {
 }
+
+NormalRoom::NormalRoom() {
+}
+
+int NormalRoom::getRoomSize() {
+	return this->roomSize;
+}
+
+void NormalRoom::enterMinibarContent(string item1, string item2, string item3) {
+	minibar[0] = item1;
+	minibar[1] = item2;
+	minibar[2] = item3;
+
+	for (int i = 0; i < 3; i++) {
+		cout << minibar[i] << " " << endl;
+	}
+}
--- hotel-booking-cpp/VipRoom.h
+++ hotel-booking-cpp/VipRoom.h
@@ -2,10 +2,18 @@
 #define VIPROOM_H
 
 #include "Room.h"
+#include "Headers.h"
 
 class VipRoom : public Room {
-
-
+private:
+	int roomSize;
+	int numOfMeals = 3;
+	string* meals = new string[numOfMeals];
+public:
+	VipRoom(int roomNum, bool isAvailable = true);
+	VipRoom();
+	void chooseRoomSize(int roomSize);
+	void whatMeals(int numOfMeals);
 };
 
 
