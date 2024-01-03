4c20f795ddd7df3022281074a7070d67db1f1dd9
Daniel <xd3nux@protonmail.com>
2024-01-03 20:27:18 +0100

    Add some new functionality to ManageInputs.

--- hotel-booking-cpp/ManageInputs.cpp
+++ hotel-booking-cpp/ManageInputs.cpp
@@ -19,18 +19,35 @@ string ManageInputs::get_string(string message, bool acceptEmptyString) {
     return theInput;
 }
 
-int ManageInputs::get_int(string message) {
-    string theInput;
-    cout << message;
-    getline(cin, theInput);
-
-    while (!is_number(theInput)) {
-        cout << "Please enter a valid number.\n";
+int ManageInputs::get_int(string message, int times) {
+    int output = 0;
+    if (times == 0) {
+        string theInput;
         cout << message;
         getline(cin, theInput);
-    }
 
-    return stoi(theInput);
+        while (!is_number(theInput)) {
+            cout << "Please enter a valid number.\n";
+            cout << message;
+            getline(cin, theInput);
+            output = stoi(theInput);
+        }
+    }
+    else {
+        for (int i = 0; i < times; i++) {
+            string theInput;
+            cout << message;
+            getline(cin, theInput);
+
+            while (!is_number(theInput)) {
+                cout << "Please enter a valid number.\n";
+                cout << message;
+                getline(cin, theInput);
+                output = stoi(theInput);
+            }
+        }
+    }
+    return output;
 }
 
 float ManageInputs::get_float(string message) {
