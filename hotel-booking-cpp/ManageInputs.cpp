17ffd4504639f1f185ac5360e4b58e2d0fb750d7
Elias Bakhshi <bakhshielias@gmail.com>
2024-01-03 21:41:59 +0100

    Polish ManageInputs::get_int()

--- hotel-booking-cpp/ManageInputs.cpp
+++ hotel-booking-cpp/ManageInputs.cpp
@@ -6,60 +6,51 @@ ManageInputs::ManageInputs() {}
 ManageInputs::~ManageInputs() {}
 
 string ManageInputs::get_string(string message, bool acceptEmptyString) {
-    string theInput;
-    cout << message;
-    getline(cin, theInput);
+	string theInput;
+	cout << message;
+	getline(cin, theInput);
 
-    // Use a loop instead of recursion
-    while (!acceptEmptyString && theInput.empty()) {
-        cout << "Please enter a non-empty string.\n";
-        cout << message;
-        getline(cin, theInput);
-    }
-    return theInput;
+	// Use a loop instead of recursion
+	while (!acceptEmptyString && theInput.empty()) {
+		cout << "Please enter a non-empty string.\n";
+		cout << message;
+		getline(cin, theInput);
+	}
+	return theInput;
 }
 
-int ManageInputs::get_int(string message, int times) {
-    int output = 0;
-    if (times == 0) {
-        string theInput;
-        cout << message;
-        getline(cin, theInput);
+int ManageInputs::get_int(string message, int minRange, int maxRange) {
+	string theInput;
+	cout << message;
+	getline(cin, theInput);
+	if (minRange == 0 && maxRange == 0) {
+		while (!is_number(theInput)) {
+			cout << "Please enter a valid number.\n";
+			cout << message;
+			getline(cin, theInput);
+		}
+	} else {
+		while (!(is_number(theInput) && stoi(theInput) >= minRange && stoi(theInput) <= maxRange)) {
+			cout << "Please enter a valid number.\n";
+			cout << message;
+			getline(cin, theInput);
+		}
+	}
 
-        while (!is_number(theInput)) {
-            cout << "Please enter a valid number.\n";
-            cout << message;
-            getline(cin, theInput);
-            output = stoi(theInput);
-        }
-    }
-    else {
-        for (int i = 0; i < times; i++) {
-            string theInput;
-            cout << message;
-            getline(cin, theInput);
+	return stoi(theInput);
 
-            while (!is_number(theInput)) {
-                cout << "Please enter a valid number.\n";
-                cout << message;
-                getline(cin, theInput);
-                output = stoi(theInput);
-            }
-        }
-    }
-    return output;
 }
 
 float ManageInputs::get_float(string message) {
-    string theInput;
-    cout << message;
-    getline(cin, theInput);
+	string theInput;
+	cout << message;
+	getline(cin, theInput);
 
-    while (!is_number(theInput)) {
-        cout << "Please enter a valid number.\n";
-        cout << message;
-        getline(cin, theInput);
-    }
+	while (!is_number(theInput)) {
+		cout << "Please enter a valid number.\n";
+		cout << message;
+		getline(cin, theInput);
+	}
 
-    return stof(theInput);
+	return stof(theInput);
 }
--- hotel-booking-cpp/ManageInputs.h
+++ hotel-booking-cpp/ManageInputs.h
@@ -9,7 +9,7 @@ public:
 	ManageInputs();
 	~ManageInputs();
 	string get_string(string message = "", bool acceptEmptyString = false);
-	int get_int(string message, int times = 0);
+	int get_int(string message, int minRange = 0, int maxRange = 0);
 	float get_float(string message);
 };
 
